8309e18601c909eb733232858fed8b161865baab
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2020-03-24 16:04:15 -0500

    basic version done

--- kwargparse.py
+++ kwargparse.py
@@ -1,3 +1,5 @@
+from argparse import Namespace
+
 #region Errors
 class KwargParseError(Exception): pass
 class ArgumentError(KwargParseError): pass
@@ -6,56 +8,6 @@ class ArgumentRequiredError(ArgumentError): pass
 #endregion
 
 _type_class = type
-
-#region Namespace class
-class _AttributeHolder(object):
-    """Abstract base class that provides __repr__.
-    The __repr__ method returns a string in the format::
-        ClassName(attr=name, attr=name, ...)
-    The attributes are determined either by a class-level attribute,
-    '_kwarg_names', or by inspecting the instance __dict__.
-    """
-
-    def __repr__(self):
-        type_name = type(self).__name__
-        arg_strings = []
-        star_args = {}
-        for arg in self._get_args():
-            arg_strings.append(repr(arg))
-        for name, value in self._get_kwargs():
-            if name.isidentifier():
-                arg_strings.append('%s=%r' % (name, value))
-            else:
-                star_args[name] = value
-        if star_args:
-            arg_strings.append('**%s' % repr(star_args))
-        return '%s(%s)' % (type_name, ', '.join(arg_strings))
-
-    def _get_kwargs(self):
-        return sorted(self.__dict__.items())
-
-    def _get_args(self):
-        return []
-
-class Namespace(_AttributeHolder):
-    """Simple object for storing attributes.
-    Implements equality by attribute names and values, and provides a simple
-    string representation.
-    NOTE:: Copied from argparse
-    """
-
-    def __init__(self, **kwargs):
-        for name in kwargs:
-            setattr(self, name, kwargs[name])
-
-    def __eq__(self, other):
-        if not isinstance(other, Namespace):
-            return NotImplemented
-        return vars(self) == vars(other)
-
-    def __contains__(self, key):
-        return key in self.__dict__
-#endregion
 
 #region Types
 def AnyType(obj):
@@ -74,8 +26,14 @@ class _NULL_RESULT: pass
 
 #region Actions
 class Action:
-    def __init__(self, names, required=True, default=None, type=AnyType):
+    def __init__(self, names, dest=None, required=None, default=None, type=AnyType):
+        if dest is None: dest = names[0]
+        if required is None:
+            if default is None:
+                required = True
+            else: required = False
         self.names = names
+        self.dest = dest
         self.required = required
         self.default = default
         self.type = type
@@ -87,14 +45,16 @@ class _Argument(Action):
         for name in self.names:
             if name in kwargs:
                 try: result = self.type(kwargs[name])
-                except ArgumentError as e:
+                except Exception as e:
                     raise e from None
                 else: break
         else:
             if self.required:
                 raise ArgumentRequiredError('argument %s required to be passed' % self.names[0]) from None
-            else: result = self.default
-        return result
+            else:
+                name = None
+                result = self.default
+        return name, result
 #endregion
 
 class KeywordArgumentParser:
@@ -102,25 +62,32 @@ class KeywordArgumentParser:
         self._args = []
         # self._error_exception = None
     @classmethod
-    def _init_as_subparser(cls, names, required=True, default=None):
+    def _init_as_subparser(cls, names, dest=None, required=None, default=None):
         self = cls()
-        Action.__init__(self, names, required, default)
+        Action.__init__(self, names, dest, required, default)
         return self
     def parse_kwargs(self, kwargs) -> Namespace:
         result = {}
+        all_names = set()
+        used = set()
         for arg in self._args:
-            _run_action(arg, kwargs)
+            all_names.update(arg.names)
+            name, result[arg.dest] = _run_action(arg, kwargs)
+            if name is not None: used.add(name)
+        extras = used - all_names
+        if extras:
+            raise IndexError('extra arguments passed: %s' % ', '.join(str(extra) for extra in extras)) from None
         return Namespace(**result)
     # def _raise_none(self): pass
     # def _raise(self, message): pass
     # def set_error_exception(self, klass, message_format=''):
     #     self._error_exception = (klass, message_format)
-    def add_argument(self, *names, dest=None, required=True, default=None, type=AnyType, action=_Argument):
-        self._args.append(action(names, required, default, type))
-    def add_subparser(self, *names, required=True, default=None):
-        subparser = self.__class__._init_as_subparser(names, required, default)
+    def add_argument(self, *names, dest=None, required=None, default=None, type=AnyType, action=_Argument):
+        self._args.append(action(names, dest, required, default, type))
+    def add_subparser(self, *names, dest=None, required=None, default=None):
+        subparser = self.__class__._init_as_subparser(names, dest, required, default)
         self._args.append(subparser)
         return subparser
     def _parse_as_arg(self, kwargs):
-        kwargs = _Argument.__call__(self, kwargs)
-        return self.parse_kwargs(kwargs)
+        name, kwargs = _Argument.__call__(self, kwargs)
+        return name, self.parse_kwargs(kwargs)
--- test.py
+++ test.py
@@ -3,14 +3,25 @@ import kwargparse
 _kwarg_parser = kwargparse.KeywordArgumentParser()
 def _init_kwarg_parser():
     _kwarg_parser.add_argument('hello')
+    subp = _kwarg_parser.add_subparser('sub', default={'world': 20})
+    subp.add_argument('world', default=20)
+    def ftemp(v):
+        if v is not None:
+            raise TypeError(v)
+    _kwarg_parser.add_argument('errors', type=ftemp, required=False)
 
 def main(**kwargs):
     result = _kwarg_parser.parse_kwargs(kwargs)
     print(result)
+    print(result.sub.world)
 
 _init_kwarg_parser()
 
 if __name__ == '__main__':
     main(
-        hello = 1
+        hello = 1,
+        sub = dict(
+            world = 2,
+        ),
+        errors = 5,
     )
